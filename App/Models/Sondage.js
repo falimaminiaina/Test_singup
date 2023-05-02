d5fab8f0ce07150cbaf4e5aafae670f58722fc73
Aina <ar@alternateeve.com>
2023-05-02 14:30:35 +0300

    vers

--- App/Models/Sondage.js
+++ App/Models/Sondage.js
@@ -2,39 +2,59 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const SondageSchema = new Schema(
     {
-        needType: {
-            type: String,
-            enum: [
-            Need_Type.PERSONAL_SPACE,
-            Need_Type.FOLDER_EMPLOYEES,
-            Need_Type.MONITORING_TIME_AND_POINTAGE,
-            Need_Type.INTERNAL_COMMUNICATION,
-            Need_Type.HR_STATISTIQUE_ANALYTICS,
-            Need_Type.MANAGEMENT_HOURS_AND_PAYS,
-            Need_Type.DOCUMENTATION,
-            Need_Type.REMBORSEMENT,
-            Need_Type.SKILLS_MANAGEMENT,
-            Need_Type.ORGANIGRAMME,
-            Need_Type.MATERIAL_INVENTORY,
-            Need_Type.PERFORMANCE_MONITORING,
-            Need_Type.FLOW_EMPLOYEES - INTEGRATION,
-            Need_Type.WORK_SCHEDULE_HOLIDAYS,
-            ],
+
+        EmployeeNumber:{
+            type: Number,
         },
-        code: {
+         language: {
             type: String,
+            default: "fr"
         },
-        question: {
-            type: String,
-        },
-        recrutment_siteCarriere: {
-            type: String,
-          enum: [
-            Recrutement_Type.ADVANCED_CAREER_SITE,
-            Recrutement_Type.RECRUITMENT_MANAGEMENT,
-            Recrutement_Type.ANALYTICAL_STATISTICS,
-            Recrutement_Type.JOBBORDE_MEASURE,
-            ],
+        dataNeed: {
+            needType: {
+                type: String,
+                enum: [
+                Need_Type.PERSONAL_SPACE,
+                Need_Type.FOLDER_EMPLOYEES,
+                Need_Type.MONITORING_TIME_AND_POINTAGE,
+                Need_Type.INTERNAL_COMMUNICATION,
+                Need_Type.HR_STATISTIQUE_ANALYTICS,
+                Need_Type.MANAGEMENT_HOURS_AND_PAYS,
+                Need_Type.DOCUMENTATION,
+                Need_Type.REMBORSEMENT,
+                Need_Type.SKILLS_MANAGEMENT,
+                Need_Type.ORGANIGRAMME,
+                Need_Type.MATERIAL_INVENTORY,
+                Need_Type.PERFORMANCE_MONITORING,
+                Need_Type.FLOW_EMPLOYEES - INTEGRATION,
+                Need_Type.WORK_SCHEDULE_HOLIDAYS,
+                ],
+                default:[]
+            },
+            code: {
+                type: String,
+            },
+            question: {
+                type: String,
+                enum:[
+                Question_type.No,
+                Question_type.Yes_fro_Recruitment,    
+                Question_type.Yes_for_employee_management,
+                Question_type.Yes_for_recruiting_and_managing_employees, 
+                ]
+            },
+            careerSite: {
+                type: String,
+              enum: [
+                Recrutement_Type.ADVANCED_CAREER_SITE,
+                Recrutement_Type.RECRUITMENT_MANAGEMENT,
+                Recrutement_Type.ANALYTICAL_STATISTICS,
+                Recrutement_Type.JOBBORDE_MEASURE,
+                ],
+             default:[]
+
+            }
+            
         }
     }
         );
